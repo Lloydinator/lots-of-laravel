b44141fbd6b48d68047de224bff509f96243134e
Lloyd Miller <lloydinator@lloyds-air.myfiosgateway.com>
2022-01-23 22:53:07 -0500

    First commit for Twilmo

---
+++ twilmo/database/migrations/2022_01_20_194521_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_from')->nullable(false)->unique()->unsigned();
+            $table->bigInteger('user_to')->nullable(false)->unique()->unsigned();
+            $table->float('amount', 8, 2)->nullable(false)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
---
+++ twilmo/database/migrations/2022_01_20_200709_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable(false)->unique()->unsigned();
+            $table->float('balance', 8, 2)->nullable(false)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
